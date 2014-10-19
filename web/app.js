aa5a3f5f844af3887fb5a1af1b165ac0a0402b81
soidid <soidid@gmail.com>
2014-10-19 16:10:17 +0800

    add semantic ui & g0v councilor info

--- web/app.js
+++ web/app.js
@@ -60,11 +60,40 @@ app.factory('DataService', function ($firebase){
 })
 
 app.controller('IndexCtrl', ['$scope', 'DataService', function ($scope, DataService){
-  
    $scope.candidates = DataService.candidates();
-   $scope.parsename = function(n){
-      return decodeURI(encodeURI(n));
+  
+   $scope.isFocused = function(name){
+      return $scope.focusedCandidate === name;
+   };
+   $scope.setFocused = function(name){
+      console.log(name);
+      $scope.focusedCandidate = name;
+   };
+   $scope.candidateChoose = function(n){
+      console.log(n);
+      $scope.focusedCandidateItem = n;
    }
+   $scope.toggleSmallPary = function(){
+      $scope.showSmallPartyOnly = !$scope.showSmallPartyOnly;
+   };
+   $scope.smallPartyFilter = function(n){
+      if($scope.showSmallPartyOnly){
+         if(n.partyEng !== 'KMT' && n.partyEng !== 'DPP'){
+            return n;
+         }
+      }else{
+         return n;
+      }
+   };
+   $scope.togglePresent = function(){
+      $scope.showPresent = !$scope.showPresent;
+   };
+   $scope.toggleSettings = function(){
+      $scope.showSettings = !$scope.showSettings;
+   };
+   $scope.togglePartyName = function(){
+      $scope.showPartyName = !$scope.showPartyName;
+   };
    
    
 }]);
@@ -72,18 +101,18 @@ app.controller('IndexCtrl', ['$scope', 'DataService', function ($scope, DataServ
 app.controller('DistrictCtrl', ['$scope',function($scope){
 
   $scope.district = 'all';
-  $scope.district = 3; //user.district
-  $scope.districts = [{"district_no":1,"district_area":'士林北投'},
-                     {"district_no":2,"district_area":'內湖南港'},
-                     {"district_no":3,"district_area":'松山信義'},
-                     {"district_no":4,"district_area":'中山大同'},
-                     {"district_no":5,"district_area":'中正萬華'},
-                     {"district_no":6,"district_area":'大安文山'},
-                     {"district_no":7,"district_area":'平地原住民'},
-                     {"district_no":8,"district_area":'山地原住民'}];
+  
+  $scope.districts = [{"district_no":1,"district_area":'士林北投',"district_candidates":21,"district_seats":13},
+                     {"district_no":2,"district_area":'內湖南港',"district_candidates":14,"district_seats":9},
+                     {"district_no":3,"district_area":'松山信義',"district_candidates":17,"district_seats":10},
+                     {"district_no":4,"district_area":'中山大同',"district_candidates":14,"district_seats":8},
+                     {"district_no":5,"district_area":'中正萬華',"district_candidates":15,"district_seats":8},
+                     {"district_no":6,"district_area":'大安文山',"district_candidates":22,"district_seats":13},
+                     {"district_no":7,"district_area":'平地原住民',"district_candidates":2,"district_seats":1},
+                     {"district_no":8,"district_area":'山地原住民',"district_candidates":3,"district_seats":1}];
 
-  $scope.selectDistrict = function(value){
-    $scope.district = value;
+  $scope.selectDistrict = function(district){
+    $scope.district = district;
 
   };
   $scope.getDistrictName = function(no){
@@ -101,15 +130,20 @@ app.controller('DistrictCtrl', ['$scope',function($scope){
     return result;
 
   };
-  $scope.isSelected = function(num){
-    return $scope.district == num;
+  $scope.isSelected = function(value){
+    if(($scope.district === 'all')||( $scope.district === 'search')){
+        return $scope.district === value;
+    }else{
+        return $scope.district.district_no === value;
+    }
+
   };
   $scope.districtFilter = function(item){
     
     if(($scope.district === 'all')||( $scope.district === 'search')){
         return item;
     }else{
-        if(item.district === $scope.district)
+        if(item.district === $scope.district.district_no)
            return item;
     }
    
