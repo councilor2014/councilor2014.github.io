3e220b6214872058e2ddd7d587b9d523fae9322b
soidid <soidid@gmail.com>
2014-10-19 16:21:53 +0800

    html5 false

--- web/app.js
+++ web/app.js
@@ -36,7 +36,7 @@ app.config(['$routeProvider','$locationProvider',
       controller: 'IndexCtrl'
     });
 
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(false);
 
   }
 ]);
